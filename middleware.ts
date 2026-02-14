d55c4c61118cd9dc3d7ac92d39874d65c3021b20
Don Cherezini <doncherezini@iMac.local>
2026-02-14 02:05:56 +0100

    Exclude sitemap.xml and robots.txt from i18n middleware

--- middleware.ts
+++ middleware.ts
@@ -4,5 +4,7 @@ import { routing } from './src/i18n/routing';
 export default createMiddleware(routing);
 
 export const config = {
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+  matcher: [
+    '/((?!api|_next|_vercel|sitemap.xml|robots.txt|.*\\..*).*)'
+  ]
 };

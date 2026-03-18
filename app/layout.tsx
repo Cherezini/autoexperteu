8acfe03ffa5e5fb557605d75a49cd4309920fd19
Don Cherezini <doncherezini@iMac.local>
2026-03-18 19:59:16 +0100

    update layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -19,6 +19,7 @@ export default function RootLayout({
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
             gtag('config', 'G-KFJSSCWV3Q');
+            gtag('config', 'AW-17987311531');
           `}
         </Script>
       </head>
